0712b3286b7a6b15fdaa84011c9ac0ec83f06bdf
karlthilking <thilking.k@northeastern.edu>
2026-01-10 00:51:56 +0000

    multi client server

--- server.c
+++ server.c
@@ -1,26 +1,22 @@
-#include <sys/socket.h> // socket, setsockopt, bind, listen, accept, AF_INET,
-                        // SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, socklen_t,
-                        // send, recv, struct sockaddr
-#include <sys/types.h> // pid_t, 
-#include <netinet/in.h> // sockaddr_in, INADDR_ANY, htons
-#include <unistd.h> // close, read, write, fork
-#include <string.h> // memset, strlen, strcat, strcpy, strdup
-#include <stdio.h> // printf
-#include <pthread.h> // pthread_t, pthread_mutex_t, pthread_mutex_lock/unlock
-                     // PTHREAD_MUTEX_INITIALIZER
-#include <stdbool.h> // bool
-#include <stdlib.h> // malloc
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-#define PORTNO 8080
+#define PORTNO  8080
 #define BACKLOG 10
-#define STDIN 0
-#define STDOUT 1
+#define STDIN   0
+#define STDOUT  1
+#define STDERR  2
 
-pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct __client_t
 {
-    char name[128];
+    char* name;
     int fd;
     struct __client_t* next;
 } client_t;
@@ -31,142 +27,112 @@ typedef struct __client_list_t
     int client_count;
 } client_list_t;
 
+pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
 client_list_t* c_list;
 
 void* add_client(client_t* client)
 {
-    bool added = false;
     pthread_mutex_lock(&client_mutex);
     
-    if (c_list->client_count < BACKLOG)
+    if (c_list->head == NULL)
     {
-        added = true;
+        c_list->head = client;
+        c_list->client_count++;
+    }
+    else if (c_list->client_count < BACKLOG)
+    {
         client->next = c_list->head;
         c_list->head = client;
         c_list->client_count++;
     }
-    
-    if (added)
-        printf("added client %d\n", client->fd);
-    else
-        printf("max clients\n");
-
+    printf("added client %s\n", client->name);
     pthread_mutex_unlock(&client_mutex);
     return NULL;
 }
 
 void* remove_client(int clientfd)
 {
-    bool removed = false;   
     pthread_mutex_lock(&client_mutex);
-
+    
+    client_t* prev = NULL;
     client_t* temp = c_list->head;
     while (temp)
     {
-        if ((temp->next)->fd == clientfd)
+        if (temp->fd == clientfd)
         {
-            removed = true;
-            temp->next = (temp->next)->next;
+            if (prev == NULL)
+                c_list->head = temp->next;
+            else
+                prev->next = temp->next;
             c_list->client_count--;
+            printf("removed client %s\n", temp->name);
         }
+        prev = temp;
+        temp = temp->next;
     }
-
-    if (removed)
-        printf("removed client %d\n", clientfd);
-    else
-        printf("could not find client %d\n", clientfd);
-
+    
     pthread_mutex_unlock(&client_mutex);
     return NULL;
 }
 
-void* display_clients()
+void* broadcast(char* msg, int sender_fd)
 {
     pthread_mutex_lock(&client_mutex);
     
-    printf("clients: ");
     client_t* temp = c_list->head;
-    while (temp)
-    {
-        if (temp->next == NULL)
-            printf("%s", temp->name);
-        else
-            printf("%s, ", temp->name);
-        temp = temp->next;
-    }
-
-    pthread_mutex_unlock(&client_mutex);
-    return NULL;
-}
-
-bool broadcast_msg(char* msg, int sender_fd)
-{
-    pthread_mutex_lock(&client_mutex);
-    client_t* temp = c_list->head;  
-    int bytes_sent;
-
     while (temp)
     {
         if (temp->fd != sender_fd)
         {
-            if ((bytes_sent = send(temp->fd, msg, strlen(msg), 0)) <= 0)
-            {
-                return false;
-            }
+            send(temp->fd, msg, strlen(msg), 0);
         }
         temp = temp->next;
     }
-    return true;
+    pthread_mutex_unlock(&client_mutex);
+    return NULL;
 }
 
 void* handle_client(client_t* client)
 {
-    char buf[256];
-    char* msg;
-    int bytes_read, bytes_sent;
-
+    char buf[512], buf2[512];
+    char* msg_header = strdup(client->name);
+    strcat(msg_header, ": ");
+    int bytes_read;
+    
     while (1)
     {
-        memset(buf, 0, 256);
-        memset(msg, 0, strlen(msg));
-
-        if ((bytes_read = recv(client->fd, buf, 255, 0)) <= 0)
+        memset(buf, 0, 512);
+        if ((bytes_read = recv(client->fd, buf, 
+                               511, 0)) <= 0)
         {
             break;
         }
-        
-        msg = strdup(client->name);
-        msg[strlen(msg)] = '\0';
-        strcat(msg, ": ");
-        strcat(msg, buf);
-        if (!broadcast_msg(msg, client->fd))
-        {
-            break;
-        }
+        strcpy(buf2, msg_header);
+        strcat(buf2, buf);
+        broadcast(buf2, client->fd);
     }
-    
+
     remove_client(client->fd);
     close(client->fd);
+    free(client->name);
     free(client);
     return NULL;
 }
 
 int main(int argc, char* argv[])
 {
-    int servfd, clientfd, opt;
+    int servfd, clientfd;
+    int opt = 1;
     struct sockaddr_in addr;
     socklen_t addrlen = sizeof(addr);
-    const char* welcome_msg = "enter your name: ";
-    char buf[512];
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORTNO);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
 
     c_list = malloc(sizeof(client_list_t));
     c_list->head = NULL;
     c_list->client_count = 0;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORTNO);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
     if ((servfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -174,7 +140,8 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    if (setsockopt(servfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    if (setsockopt(servfd, SOL_SOCKET, SO_REUSEADDR,
+                   &opt, sizeof(opt)) == -1)
     {
         perror("setsockopt");
         exit(1);
@@ -192,31 +159,32 @@ int main(int argc, char* argv[])
         exit(1);
     }
     
+    char buf[512];
     while (1)
     {
-        if (c_list->client_count == 2)
-            display_clients();
         memset(buf, 0, 512);
-        if ((clientfd = accept(servfd, (struct sockaddr*)&addr, &addrlen)) == -1)
+        clientfd = accept(servfd,(struct sockaddr*)&addr,
+                          &addrlen);
+        if (clientfd == -1)
         {
             perror("accept");
             continue;
         }
 
-        send(clientfd, welcome_msg, strlen(welcome_msg), 0);
+        send(clientfd, "enter your name: ", 18, 0);
         recv(clientfd, buf, 511, 0);
-        buf[strlen(buf)] = '\0';
+        buf[strlen(buf) - 2] = '\0';
 
         client_t* client = malloc(sizeof(client_t));
-        strcpy(client->name, buf);
+        client->name = strdup(buf);
         client->fd = clientfd;
         add_client(client);
-        
+
         pthread_t th;
-        pthread_create(&th, NULL, (void*)handle_client, client);
+        pthread_create(&th, NULL, (void*)handle_client,
+                       client);
         pthread_detach(th);
     }
-
-    close(servfd);
     return 0;
 }
+
